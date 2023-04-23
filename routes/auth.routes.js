d12bddf3d1d8c8250dceff1d08129828d5468c6a
Natassa <112552395+natko22@users.noreply.github.com>
2023-04-23 15:42:00 +0200

    itration 1 done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,12 +3,15 @@ const User = require("../models/User.model");
 const router = require("express").Router();
 const saltRounds = 12;
 
-
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
-router.post("/signup", async (req,res,next)=>{
+// router.get("/profile", (req, res) => {
+//   res.render("auth/profile");
+// });
+
+router.post("/signup", async (req, res, next) => {
   // const{email,password} = req.body;
 
   const salt = await bcryptjs.genSalt(saltRounds);
@@ -17,18 +20,11 @@ router.post("/signup", async (req,res,next)=>{
   const hash = await bcryptjs.hash(req.body.password, salt);
   console.log(hash);
 
-  const newUser = new User({ username : req.body.username, password: hash });
+  const newUser = new User({ username: req.body.username, password: hash });
   console.log(newUser);
   await newUser.save();
 
   res.redirect("/profile");
 });
-
-// router.get("/profile", (req, res) => {
-//   res.render("auth/profile");
-// });
-
-
-
 
 module.exports = router;
